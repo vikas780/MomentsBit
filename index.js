dd15ba0c84ec9f3d167e8ffebda7e5243a0f19bb
Adarsh2908 <adarshsimar12345@gmail.com>
2021-08-16 14:00:54 +0530

    Added new File Structure

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 // PACKAGE IMPORT
 const express = require('express')
 require('dotenv').config()
+const path = require('path')
+const hbs = require('hbs')
 
 
 // VARIABLES
@@ -18,12 +20,46 @@ const PORT = process.env.PORT || 3000
 
 // MAIN STRUCTURE OF MOMENTS BIT (INDEX.JS) 
 
+const views_path = path.join(__dirname, 'Views/layouts/')
+
+// SETTING UP PIPELINE 
+app.set("view engine", "hbs")
+app.set("views",'views_path')
+
+
+// GETS
+
+// LANDING PAGE 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send("index")
+})
+
+
+// HOMEPAGE 
+app.get('/homepage',(req,res) => {
+    res.send("homepage")
+})
+
+// lOGIN PAGE 
+app.get('/login',(req,res) => {
+    res.send("login")
+})
+
+// REGISTER PAGE 
+app.get('/register',(req,res) => {
+    res.send("register")
+})
+
+
+// PASSWORD RECOVERY PAGE 
+app.get('/passwordrecovery',(req,res) => {
+    res.send("passwordrecovery")
 })
 
 
 
+
+// PORT
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`)
 })
