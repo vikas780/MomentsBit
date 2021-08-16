f3b55c0abfaeb56c99d8b513109ad63e5509adc7
vikas780 <vikas1000dhiman@gmail.com>
2021-08-16 15:49:00 +0530

    Routing Done

--- index.js
+++ index.js
@@ -14,48 +14,40 @@ const PORT = process.env.PORT || 3000;
 
 // MAIN STRUCTURE OF MOMENTS BIT (INDEX.JS)
 
+// MAIN STRUCTURE OF MOMENTS BIT (INDEX.JS)
+
 const views_path = path.join(__dirname, "Views/layouts/");
 
-// MAIN STRUCTURE OF MOMENTS BIT (INDEX.JS) 
-
-const views_path = path.join(__dirname, "Views/layouts/")
-
-// SETTING UP PIPELINE 
-app.set("view engine", "hbs")
-app.set("views",views_path)
-
+// SETTING UP PIPELINE
+app.set("view engine", "hbs");
+app.set("views", views_path);
 
 // GETS
 
-// LANDING PAGE 
-app.get('/', (req, res) => {
-  res.render("index")
-})
+// LANDING PAGE
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
+// HOMEPAGE
+app.get("/homepage", (req, res) => {
+  res.render("homepage");
+});
 
-// HOMEPAGE 
-app.get('/homepage',(req,res) => {
-    res.render("homepage")
-})
+// lOGIN PAGE
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
-// lOGIN PAGE 
-app.get('/login',(req,res) => {
-    res.render("login")
-})
+// REGISTER PAGE
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 
-// REGISTER PAGE 
-app.get('/register',(req,res) => {
-    res.render("register")
-})
-
-
-// PASSWORD RECOVERY PAGE 
-app.get('/passwordrecovery',(req,res) => {
-    res.render("passwordrecovery")
-})
-
-
-
+// PASSWORD RECOVERY PAGE
+app.get("/passwordrecovery", (req, res) => {
+  res.render("passwordrecovery");
+});
 
 // PORT
 app.listen(PORT, () => {
